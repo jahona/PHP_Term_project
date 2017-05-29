aba526fdb94ac6815360a0f5347f508ed5368676
SJR\kadam <kadamon2007@gmail.com>
2017-05-29 14:05:33 +0900

    change index.php // login_form.php // member_form.php

--- member/member_form.php
+++ member/member_form.php
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<link href="../css/common.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="../assets/css/main.css" />
 <link href="../css/member.css" rel="stylesheet" type="text/css" media="all">
 <script>
    function check_id()
@@ -97,74 +97,85 @@
       return;
    }
 </script>
+<style media="screen">
+	input::-webkit-input-placeholder { color:#111111 }
+</style>
 </head>
 
 <body>
-<div id="wrap">
-  <div id="header">
-    <? include "../lib/top_login2.php"; ?>
-  </div>  <!-- end of header -->
+	<div class="page-wrap">
 
-  <div id="menu">
-	<? include "../lib/top_menu2.php"; ?>
-  </div>  <!-- end of menu -->
-
-  <div id="content">
-	<div id="col1">
-		<div id="left_menu">
-<?
-			include "../lib/left_menu.php";
-?>
-		</div>
-	</div> <!-- end of col1 -->
-
-	<div id="col2">
-        <form  name="member_form" method="post" action="insert.php">
-		<div id="title">
-			<img src="../img/title_join.gif">
-		</div>
-
-		<div id="form_join">
-			<div id="join1">
-			<ul>
-			<li>* 아이디</li>
-			<li>* 비밀번호</li>
-			<li>* 비밀번호 확인</li>
-			<li>* 이름</li>
-			<li>* 닉네임</li>
-			<li>* 휴대폰</li>
-			<li>&nbsp;&nbsp;&nbsp;이메일</li>
+		<!-- nav -->
+		<nav id="nav">
+    		<? include "../lib/top_login1.php"; ?>
 			</ul>
-			</div>
-			<div id="join2">
-			<ul>
-			<li><div id="id1"><input type="text" name="id"></div><div id="id2"><a href="#"><img src="../img/check_id.gif" onclick="check_id()"></a></div><div id="id3">4~12자의 영문 소문자, 숫자와 특수기호(_) 만 사용할 수 있습니다.</div></li>
-			<li><input type="password" name="pass"></li>
-			<li><input type="password" name="pass_confirm"></li>
-			<li><input type="text" name="name"></li>
-			<li><div id="nick1"><input type="text" name="nick"></div><div id="nick2" ><a href="#"><img src="../img/check_id.gif" onclick="check_nick()"></a></div></li>
-			<li><select class="hp" name="hp1">
-              <option value='010'>010</option>
-              <option value='011'>011</option>
-              <option value='016'>016</option>
-              <option value='017'>017</option>
-              <option value='018'>018</option>
-              <option value='019'>019</option>
-						</select>  - <input type="text" class="hp" name="hp2" maxlength="4"> - <input type="text" class="hp" name="hp3" maxlength="4"></li>
-			<li><input type="text" id="email1" name="email1"> @ <input type="text" name="email2"></li>
-			</ul>
-			</div>
-			<div class="clear"></div>
-			<div id="must"> * 는 필수 입력항목입니다.^^</div>
-		</div>
+		</nav>
 
-		<div id="button"><a href="#"><img src="../img/button_save.gif"  onclick="check_input()"></a>&nbsp;&nbsp;
-		                 <a href="#"><img src="../img/button_reset.gif" onclick="reset_form()"></a>
-		</div>
-	    </form>
-	</div> <!-- end of col2 -->
-  </div> <!-- end of content -->
-</div> <!-- end of wrap -->
+		<!-- Main -->
+			<section id="main">
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+						<div class="column" style="display: block; width: 900px; margin:0 auto; position:relative;">
+							<form name="member_form" method="post" action="insert.php">
+								<div class="field half first">
+									<h3>회원가입</h3>
+									<div class="form_join">
+
+
+									<div id="id1" style="float: left; width:80%;">
+										<input type="text" name="id" placeholder="ID를 입력해주세요" style="width: 240pt">
+									</div>
+									<div id="id2" style="float: left; width:20%;">
+										<a href="#"><img src="../img/check_id.gif" onclick="check_id()"></a>
+										<br><br>
+									</div>
+									<div>
+										<input type="password" name="pass" placeholder="비밀번호를 입력해주세요">
+									</div>
+									<br>
+									<input type="password" name="pass_confirm" placeholder="비밀번호를 다시 입력해주세요">
+									<br>
+									<input type="text" name="name"  placeholder="이름을 입력해주세요">
+									<br>
+									<input type="text" name="nick"  placeholder="닉네임을 입력해주세요">
+									</div>
+
+								</div>
+
+								<div>
+									<div id="join2">
+									<ul>
+									<li><div id="id1"><input type="text" name="id"></div><div id="id2"><a href="#"><img src="../img/check_id.gif" onclick="check_id()"></a></div><div id="id3">4~12자의 영문 소문자, 숫자와 특수기호(_) 만 사용할 수 있습니다.</div></li>
+								</div>
+								</div>
+
+								<div id="button"><a href="#"><img src="../img/button_save.gif"  onclick="check_input()"></a>&nbsp;&nbsp;
+								                 <a href="#"><img src="../img/button_reset.gif" onclick="reset_form()"></a>
+								</div>
+							    </form>
+						</div>
+					</section>
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="copyright">
+							&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+						</div>
+					</footer>
+			</section>
+	</div>
+
+	<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.poptrox.min.js"></script>
+		<script src="assets/js/jquery.scrolly.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+
+
+
 
 </body>
 </html>
