a847cdff036e327a7f3596315d631203394def46
SJR\kadam <kadamon2007@gmail.com>
2017-06-04 00:37:14 +0900

    bugfix_free/list.php

--- free/list.php
+++ free/list.php
@@ -3,12 +3,35 @@
 	$table = "free";
 	$ripple = "free_ripple";
 ?>
+<?
+    function getImage() {
+      include "../lib/dbconn.php";
+
+      $sql = "select * from free";
+      $result = mysql_query($sql, $connect);
+
+      $count = 0;
+
+      while($row = mysql_fetch_array($result)) {
+        $obj[$count] = (object)array('id' => $row[id], 'nick' => $row[nick], 'file_copied' => $row[file_copied_0]);
+        $count++;
+      }
+
+      // for($i=0 ; $i<$count ; $i++) {
+      //   echo $obj[$i]->id." ".$obj[$i]->nick." ".$obj[$i]->file_name."<br />";
+      // }
+
+      mysql_close();
+
+			return $obj;
+    }
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta charset="utf-8">
-<link href="../css/common.css" rel="stylesheet" type="text/css" media="all">
-<link href="../css/board4.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" href="../assets/css/main.css" />
 </head>
 <?
 	include "../lib/dbconn.php";
@@ -50,128 +73,105 @@
 	$number = $total_record - $start;
 ?>
 <body>
-<div id="wrap">
-  <div id="header">
-    <? include "../lib/top_login2.php"; ?>
-  </div>  <!-- end of header -->
-  <div id="menu">
-	<? include "../lib/top_menu2.php"; ?>
-  </div>  <!-- end of menu -->
+	<div class="page-wrap">
 
-  <div id="content">
-	<div id="col1">
-		<div id="left_menu">
-<?
-			include "../lib/left_menu.php";
-?>
-		</div>
+		<!-- nav -->
+		<nav id="nav">
+				<? include "../lib/top_login1.php"; ?>
+		</nav>
+
+		<!-- Main -->
+			<section id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<div>Snapshot <span>by TEMPLATED</span></div>
+					</header>
+
+				<!-- Gallery -->
+					<section id="galleries">
+
+						<!-- Photo Galleries -->
+							<div class="gallery">
+
+								<!-- Filters -->
+									<header>
+										<h1>Gallery</h1>
+										<ul class="tabs">
+											<li><a href="#" data-tag="all" class="button active">All</a></li>
+											<li><a href="#" data-tag="people" class="button">People</a></li>
+											<li><a href="#" data-tag="place" class="button">Places</a></li>
+											<li><a href="#" data-tag="thing" class="button">Things</a></li>
+										</ul>
+									</header>
+
+									<div class="content">
+										<?
+											$obj = getImage();
+													for($i=0; $i<8; $i++){
+															$str = "<div class='media all people'>"."<a href='클릭시 경로'><img src='./data/".$obj[$i]->file_copied."' alt='' title='This right here is a caption.' /></a>
+															</div>";
+
+															echo $str;
+															echo $obj[0]->file_name;
+													}
+										?>
+
+									</div>
+							</div>
+					</section>
+
+
+
+								<div id="page_button">
+									<div id="page_num"> ◀ 이전 &nbsp;&nbsp;&nbsp;&nbsp;
+					<?
+					   // 게시판 목록 하단에 페이지 링크 번호 출력
+					   for ($i=1; $i<=$total_page; $i++)
+					   {
+							if ($page == $i)     // 현재 페이지 번호 링크 안함
+							{
+								echo "<b> $i </b>";
+							}
+							else
+							{
+								echo "<a href='list.php?table=$table&page=$i'> $i </a>";
+							}
+					   }
+					?>
+								&nbsp;&nbsp;&nbsp;&nbsp;다음 ▶
+									</div>
+									<div id="button">
+										<a href="list.php?table=<?=$table?>&page=<?=$page?>"><img src="../img/list.png"></a>&nbsp;
+					<?
+						if($userid)
+						{
+					?>
+							<a href="write_form.php?table=<?=$table?>"><img src="../img/write.png"></a>
+					<?
+						}
+					?>
+					</div>
+					</div>
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="copyright">
+							&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.
+						</div>
+					</footer>
+			</section>
 	</div>
 
-	<div id="col2">
-		<div id="title">
-			<img src="../img/title_free.gif">
-		</div>
 
-		<form  name="board_form" method="post" action="list.php?table=<?=$table?>&mode=search">
-		<div id="list_search">
-			<div id="list_search1">▷ 총 <?= $total_record ?> 개의 게시물이 있습니다.  </div>
-			<div id="list_search2"><img src="../img/select_search.gif"></div>
-			<div id="list_search3">
-				<select name="find">
-                    <option value='subject'>제목</option>
-                    <option value='content'>내용</option>
-                    <option value='nick'>별명</option>
-                    <option value='name'>이름</option>
-				</select></div>
-			<div id="list_search4"><input type="text" name="search"></div>
-			<div id="list_search5"><input type="image" src="../img/list_search_button.gif"></div>
-		</div>
-		</form>
-		<div class="clear"></div>
+<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.poptrox.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
 
-		<div id="list_top_title">
-			<ul>
-				<li id="list_title1"><img src="../img/list_title1.gif"></li>
-				<li id="list_title2"><img src="../img/list_title2.gif"></li>
-				<li id="list_title3"><img src="../img/list_title3.gif"></li>
-				<li id="list_title4"><img src="../img/list_title4.gif"></li>
-				<li id="list_title5"><img src="../img/list_title5.gif"></li>
-			</ul>
-		</div>
 
-		<div id="list_content">
-<?
-   for ($i=$start; $i<$start+$scale && $i < $total_record; $i++)
-   {
-      mysql_data_seek($result, $i);     // 포인터 이동
-      $row = mysql_fetch_array($result); // 하나의 레코드 가져오기
-
-	  $item_num     = $row[num];
-	  $item_id      = $row[id];
-	  $item_name    = $row[name];
-  	  $item_nick    = $row[nick];
-	  $item_hit     = $row[hit];
-      $item_date    = $row[regist_day];
-	  $item_date = substr($item_date, 0, 10);
-	  $item_subject = str_replace(" ", "&nbsp;", $row[subject]);
-
-	  $sql = "select * from $ripple where parent=$item_num";
-	  $result2 = mysql_query($sql, $connect);
-	  $num_ripple = mysql_num_rows($result2);
-
-?>
-			<div id="list_item">
-				<div id="list_item1"><?= $number ?></div>
-				<div id="list_item2"><a href="view.php?table=<?=$table?>&num=<?=$item_num?>&page=<?=$page?>"><?= $item_subject ?></a>
-<?
-		if ($num_ripple)
-				echo " [$num_ripple]";
-?>
-				</div>
-				<div id="list_item3"><?= $item_nick ?></div>
-				<div id="list_item4"><?= $item_date ?></div>
-				<div id="list_item5"><?= $item_hit ?></div>
-			</div>
-<?
-   	   $number--;
-   }
-?>
-			<div id="page_button">
-				<div id="page_num"> ◀ 이전 &nbsp;&nbsp;&nbsp;&nbsp;
-<?
-   // 게시판 목록 하단에 페이지 링크 번호 출력
-   for ($i=1; $i<=$total_page; $i++)
-   {
-		if ($page == $i)     // 현재 페이지 번호 링크 안함
-		{
-			echo "<b> $i </b>";
-		}
-		else
-		{
-			echo "<a href='list.php?table=$table&page=$i'> $i </a>";
-		}
-   }
-?>
-			&nbsp;&nbsp;&nbsp;&nbsp;다음 ▶
-				</div>
-				<div id="button">
-					<a href="list.php?table=<?=$table?>&page=<?=$page?>"><img src="../img/list.png"></a>&nbsp;
-<?
-	if($userid)
-	{
-?>
-		<a href="write_form.php?table=<?=$table?>"><img src="../img/write.png"></a>
-<?
-	}
-?>
-				</div>
-			</div> <!-- end of page_button -->
-        </div> <!-- end of list content -->
-		<div class="clear"></div>
-
-	</div> <!-- end of col2 -->
-  </div> <!-- end of content -->
-</div> <!-- end of wrap -->
 
 </body>
 </html>
